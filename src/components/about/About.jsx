05987a891f27216fb2fb39fe8d7de7260b960dc4
Axidevs <aayushi.ch81@gmail.com>
2026-07-21 16:21:37 +0530

    creating home component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,6 +6,65 @@ const About = () => {
   return (
     <div id = 'about'>
       
+<div className="leftabout">
+
+<div className="info1">
+
+
+<div className="box1"></div>
+
+
+<div className="personalinfo">
+<h2>Personal Info</h2>
+<p> <span>NAME:</span> AAYUSHI CHAUDHARY</p>
+<p> <span>GENDER:</span> FEMALE</p>
+<p><span>Email:</span> aayushi.ch81@gmail.com</p>
+<p> <span>GitHub:</span> github.com/Axidevs</p>
+</div>
+</div>
+
+<div className="info2">
+<div className="box2"></div>
+
+<div className="education">
+<h2>Education</h2>
+<p> <span>DEGREE:</span> BTECH</p>
+<p> <span>COLLEGE-NAME:</span> BPIT , DELHI</p>
+<p> <span>BRANCH:</span> INFORMATION TECHNOLOGY</p>
+<p> <span>DURATION: </span>2025 - 2029</p>
+</div>
+</div>
+
+<div className="info3">
+<div className="box3"></div>
+
+<div className="skills">
+
+<h2>Skills</h2>
+<p>WEB DEVELOPER </p>
+<p>REACT DEVELOPER</p>
+<p>JAVA</p>
+</div>
+</div>
+
+
+
+
+
+
+</div>
+
+<div className="rightabout">
+
+
+
+</div>
+
+
+
+
+
+
 
 
 
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div id ="card">
+      
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Card
