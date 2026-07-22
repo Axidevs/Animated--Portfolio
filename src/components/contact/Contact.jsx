f7808b926127af47135f2139410dda051bc81bd9
Axidevs <aayushi.ch81@gmail.com>
2026-07-22 22:56:20 +0530

    create contact-component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import './Contact.css'
+
+import contact from "../../assets/contact.png"
+import Card from '../card/Card'
+
+
 const Contact = () => {
   return (
     <div id = "contact">
       
+<div className="leftcontact">
+
+<img src = {contact} alt='img'/>
+
+</div>
+
+<div className="rightcontact">
+
+<form action = 'https://formspree.io/f/mdaqlkbj' method="POST">
+
+<input type='text' placeholder='Name' name='Username'/>
+<input type='email' placeholder='Email' name='Email'/>
+<textarea
+  className="msgbox"
+  placeholder="Type your message..."
+></textarea>
+<button id='btn'>Submit</button>
+
+
+
+
+</form>
+
+
+
+
+
+
+</div>
+
+
+
     </div>
   )
 }
--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -3,8 +3,8 @@ import "./Project.css"
 import tic from "../../assets/tic tac toe.jpeg"
 import vi from "../../assets/vii.png"
 import cc from "../../assets/cc.png"
-import food from "../../assets/food.png"
-import ai from "../../assets/ai.png"
+import image from "../../assets/food.png"
+import ai from "../../assets/ise.png"
 import website from "../../assets/website.png"
 
 import './Project.css'
@@ -26,7 +26,7 @@ const Project = () => {
 <Card title= " Currency Convertor" image = {cc}/>
 
 <Card title ="Virtual Assistant " image = {vi}/>
-<Card title= "Food Delivery Website" image = {food} />
+<Card title= "Food Delivery Website" image = {image} />
 <Card title="AI Chatbot " image = {ai}/>
 
 
